f8d73c9037d7cdeab1c8f5782d7d27162d86659e
Dxh7788 <xiaohong.dong@htouhui.com>
2018-01-02 15:19:51 +0800

    issue #1 redis的3种连接方式

--- src/test/java/djredis/period/one/CommonJedisTest.java
+++ src/test/java/djredis/period/one/CommonJedisTest.java
@@ -18,9 +18,28 @@ public class CommonJedisTest {
     public void testCon(){
         /**
          * common config,require host/auth password
+         * 1.connection manner 1
+         * host=localtest or ips like 127.0.0.1
+         * port = 6379 ,6379 is the default port
+         * the constructor is Jedis(host,port)
          * */
-        Jedis jedis = new Jedis("localhost");
+        /*Jedis jedis = new Jedis("localhost");
         jedis.auth("foobared");
-        jedis.set("jedis_test","ceshi");
+        jedis.set("jedis_test","ceshi");*/
+        /**
+         * 1.1 connection manner 2
+         * */
+        /*Jedis jedis1 = new Jedis("192.168.0.109");
+        jedis1.auth("foobared");
+        jedis1.set("jedis_test_manner2","ceshi2");*/
+        /**
+         * 2.connection manner 2
+         * connected the redis client with the given URI started with scheme "redis" like 'redis://www.htouhui.com/redis-cluster'
+         * 2.1  Jedis jedis2 = new Jedis("redis://127.0.0.1:6379/0");
+         *      jedis2.auth("foobared");
+         * 2.2  Jedis jedis2 = new Jedis("redis://user:foobared@127.0.0.1:6379/0");
+         * */
+        Jedis jedis2 = new Jedis("redis://requirepass:foobared@127.0.0.1:6379/0");
+        jedis2.set("jedis_test_manner4","ceshi4");
     }
 }
